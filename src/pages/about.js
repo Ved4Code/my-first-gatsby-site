61dd0102143de2f5eb81a0ccb1e9087878b028ac
Your Name <you@example.com>
2021-07-01 22:13:43 +0530

    Customized by me

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,21 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const AboutPage = () => {
   return (
     <Layout pageTitle="About Me">
-      <p>Hi there! I'm the proud creator of this site, which I built with Gatsby.</p>
+      <p>Hi there! I'm the proud creator of this site. Myself <i>Vedant Sultania</i> pursuing <i>B.tech in Chem Engg.</i> from <i>IIT BOMBAY</i>
+          ..Recently I completed my 2nd yr. and to be in my 3rd yr. soon.Talking about my hobbies I like reading books,watching movies,dramatics,
+            & playing outdoor games(especially cricket:D).Also,I love travelling very much and spending time with family and friends ❤❤.
+                Always ready to learn and explore something new !!💫  </p>
+      <StaticImage
+        alt="Photo of website creator"
+        src="../images/My_pic.jpg"
+      />
+      <p>Contact 📱: 8651813603  📩:vedantsultania36@gmail.com 
+            Feel free to reach out to me!!
+      </p>
     </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,10 +5,12 @@ import { StaticImage } from 'gatsby-plugin-image'
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Hello everyone! I am making my own blog site which is going to be published very soon. Loving the every new technology I explore.
+        Hoping to make this amazing website published soon having a lot of features. Mean while you can know more about me in <b>ABOUT</b> section.
+        Updating blog page very soon. Stay Tuned ! 😀✌ </p>
       <StaticImage
-        alt="Clifford, a reddish-brown pitbull, dozing in a bean bag chair"
-        src="../images/home_image.jpg"
+        alt="A person trying to build websites."
+        src="../images/home_tech.jpg"
       />
     </Layout>
   )
